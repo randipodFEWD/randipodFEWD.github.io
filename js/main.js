efe2756e0490159a8e49252c89c103568a3f2aa3
randipodFEWD <randipod@gmail.com>
2016-04-17 19:46:36 -0400

    more nav drama

--- js/main.js
+++ js/main.js
@@ -4,4 +4,21 @@ $(function(){
   $('#hamburger').click(function(){
     $("#mobile-nav").toggleClass('removeThis');
   })
+
+	$("#hamburger").click(function(){
+      if($("#mobile-nav").hasClass('hide')) {
+        $("#mobile-nav").animate({height:0},200).removeClass('hide');
+      } else { 
+        $("#mobile-nav").animate({height:32},200).addClass('hide');
+      }
+    });
 });
+
+
+
+
+ //   $('#hamburger').click(function(){
+	//    $("#mobile-nav").toggle(function(){
+	//     $(this).animate({height: '2em'},200);
+	//   });
+	// })
