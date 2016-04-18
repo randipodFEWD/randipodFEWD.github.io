8c1ed2438fa8599ea4691d900310923241cfd98a
randipodFEWD <randipod@gmail.com>
2016-04-17 23:57:34 -0400

    nav again!

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 $(function(){
-	console.log('ready!');
-
-  $('#hamburger').click(function(){
-    $("#mobile-nav").toggleClass('removeThis');
-  })
-
-	$("#hamburger").click(function(){
-      if($("#mobile-nav").hasClass('hide')) {
-        $("#mobile-nav").animate({height:0},200).removeClass('hide');
-      } else { 
-        $("#mobile-nav").animate({height:32},200).addClass('hide');
-      }
-    });
+  // hide mobile nav when page loads
+  $('#mobile-nav').hide();
+  $("#hamburger").click(function(){
+    $("#mobile-nav").slideToggle("400ms","linear");
+  });
 });
+
+//   $('#hamburger').click(function(){
+//     $("#mobile-nav").toggleClass('removeThis');
+//   })
+
+// 	$("#hamburger").click(function(){
+//       if($("#mobile-nav").hasClass('hide')) {
+//         $("#mobile-nav").animate({height:0},200).removeClass('hide');
+//       } else { 
+//         $("#mobile-nav").animate({height:32},200).addClass('hide');
+//       }
+//     });
+// });
 
 
 
